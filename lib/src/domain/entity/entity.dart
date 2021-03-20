b57853ef3c466d9d1b95acfce8d4afd3323c74d7
Moustafa Essam <drsh_201122@yahoo.com>
2021-03-21 01:00:08 +0200

    initial

--- lib/src/domain/entity/entity.dart
+++ lib/src/domain/entity/entity.dart
@@ -2,5 +2,8 @@ import 'package:equatable/equatable.dart';
 
 abstract class Entity extends Equatable {
   const Entity();
+
+  get stringify => true;
+
   List<String> get serverSuccessMessages => null;
 }
